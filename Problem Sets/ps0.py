3118a0106ddc7c360a0de587d96663f0b81bd0d0
Alec Dewulf <56365692+AWD81@users.noreply.github.com>
2020-01-03 11:09:24 -0500

    Solution to problem set 0

---
+++ Problem Sets/ps0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Alec Deuwlf
+Problem Set 0
+Time Spent: 0:05
+Created on Fri Jan  3 10:59:26 2020
+
+"""
+import math
+
+x = int(input("enter a number x "))
+y = int(input("enter a number y "))
+
+print("X**Y = ", x **y)
+print("log(x) =", math.log(2, x))
